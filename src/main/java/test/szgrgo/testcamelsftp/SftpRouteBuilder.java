fce3201fcfe6f608ee04a258b02d187944a18bbd
Gergő Szabó <szgrgo2@gmail.com>
2023-03-12 20:19:10 +0100

    Rebase from master

--- src/main/java/test/szgrgo/testcamelsftp/SftpRouteBuilder.java
+++ src/main/java/test/szgrgo/testcamelsftp/SftpRouteBuilder.java
@@ -1,17 +1,34 @@
 package test.szgrgo.testcamelsftp;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.apache.camel.AggregationStrategy;
+import org.apache.camel.CamelContext;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.idempotent.hazelcast.HazelcastIdempotentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SftpRouteBuilder extends RouteBuilder {
+
+    @Autowired
+    private CamelContext camelContext;
+
     @Override
     public void configure() {
         AggregationStrategy aggregationStrategy = new MultiLineAggregationStrategy();
-        from(sftpWithIdempotentRepository())
+
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        HazelcastIdempotentRepository idempotentRepository = new HazelcastIdempotentRepository(hazelcastInstance);
+        camelContext.getRegistry().bind("idempotentRepository",idempotentRepository);
+
+        from(sftpWithReadLockIdempotency())
+
                .routeId("pollSftpRoute1")
+               .idempotentConsumer(header("CamelFileName"), idempotentRepository)
                .log(LoggingLevel.INFO, "File ${file:name} downloaded")
                .split(body().tokenize("\n"))
                 .aggregate(aggregationStrategy)
@@ -19,7 +36,7 @@ public class SftpRouteBuilder extends RouteBuilder {
                .to("kafka:test?brokers=localhost:9094");
     }
 
-    private static String sftpWithIdempotentRepository() {
-        return "sftp://localhost:22?username=demo&password=demo";
+    private static String sftpWithReadLockIdempotency() {
+        return "sftp://localhost:22?username=demo&password=demo&readLock=idempotent&readLockLoggingLevel=INFO&idempotentRepository=#idempotentRepository";
     }
 }
